2218b9458af3366da23e7d233bf394ad6ad3f625
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-08-10 18:17:47 +0200

    added fonts folder to styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import { ProviderWrapper } from "@/ProviderWrapper/ProviderWrapper.provider";
 import StyledComponentsRegistry from "@/lib/registry";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -1,10 +1,11 @@
 import { createGlobalStyle } from "styled-components";
+import { roboto_mono } from ".";
 
 export const GlobalStyle = createGlobalStyle`
   body {
     padding:0;
     margin:0;
     background-color:black;
-    color:white;
+    color:white;  
   }
 `;
